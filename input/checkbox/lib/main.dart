718d406d0aa447cab86bf4c30732f7fb67a52dd9
Alfian Yusuf Abdullah <alfianyabdullah@gmail.com>
2018-08-21 10:21:14 +0700

    add radion sample

---
+++ input/checkbox/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _state = false;
+
+  void _setChecked(bool value){
+    setState(() => _state = value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(_state ? "Checked" : "Not Cheched"),
+            Checkbox(value: _state, onChanged: (bool value) => _setChecked(value))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ input/radio/lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _radioGroupValue = 0;
+  String _selected = "Hello Flutter";
+
+  void _select(int value){
+    _radioGroupValue = value;
+
+    setState(() {
+      switch(_radioGroupValue){
+        case 0:
+          _selected = "Android Selected";
+          break;
+        case 1:
+          _selected = "iOS Selected";
+          break;
+        case 2:
+          _selected = "Fuchsia Selected";
+          break;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                _selected,
+                style: TextStyle(fontSize: 25.0),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Radio(
+                  value: 0,
+                  groupValue: _radioGroupValue,
+                  onChanged: (value) {
+                    _select(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Text('Android'),
+                ),
+                Radio(
+                  value: 1,
+                  groupValue: _radioGroupValue,
+                  onChanged: (value) {
+                    _select(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Text('iOS'),
+                ),
+                Radio(
+                  value: 2,
+                  groupValue: _radioGroupValue,
+                  onChanged: (value) {
+                    _select(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Text('Fuchsia'),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
